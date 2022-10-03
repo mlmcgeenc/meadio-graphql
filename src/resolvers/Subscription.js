ddc28e3cab1121ee494457df6e562bc1c6cab104
Matt McGee <mlmcgeenc@gmail.com>
2022-10-03 19:20:10 -0400

    add the first gql subscription

---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,20 @@
+import { PubSub } from "graphql-yoga"
+
+const Subscription = {
+  count: {
+    subscribe(parent, args, { pubsub }, info) {
+      let count = 0
+
+      setInterval(()=> {
+        count++
+        pubsub.publish("count", {
+          count: count
+        })
+      }, 1000)
+
+      return pubsub.asyncIterator("count")
+    }
+  }
+}
+
+export { Subscription as default }
