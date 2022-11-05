7f51db1d18d4379bfaa75eadbb75121a2ac4f011
Matt McGee <mlmcgeenc@gmail.com>
2022-11-05 17:38:25 -0400

    add subscriptions to creat, update, delete of comments

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -80,15 +80,21 @@ const Mutation = {
 		};
 
 		db.posts.push(post);
-    if (args.data.published) {
-      pubsub.publish(`post`, { post: post });
-    }
-  
+		if (args.data.published) {
+			pubsub.publish(`post`, {
+				post: {
+					mutation: "CREATED",
+					data: post,
+				},
+			});
+		}
 
 		return post;
 	},
-	updatePost(parent, { id, data }, { db }, info) {
+	updatePost(parent, { id, data }, { db, pubsub }, info) {
 		const post = db.posts.find((post) => post.id === id);
+		const originalPost = { ...post }; // object spread operator to clone post object
+
 		if (!post) {
 			throw new Error("Post not found");
 		}
@@ -104,6 +110,29 @@ const Mutation = {
 
 		if (typeof data.published === "boolean") {
 			post.published = data.published;
+
+			if (originalPost.published && !post.published) {
+				pubsub.publish("post", {
+					post: {
+						mutation: "UPDATE - UNPUBLISHED",
+						data: originalPost,
+					},
+				});
+			} else if (!originalPost.published && post.published) {
+				pubsub.publish("post", {
+					post: {
+						mutation: "UPDATE - PUBLISHED",
+						data: post,
+					},
+				});
+			}
+		} else if (post.published) {
+			pubsub.publish("post", {
+				post: {
+					mutation: "UPDATED - CONTENT",
+					data: post,
+				},
+			});
 		}
 
 		if (typeof data.author === "string") {
@@ -112,17 +141,27 @@ const Mutation = {
 
 		return post;
 	},
-	deletePost(parent, args, { db }, info) {
+	deletePost(parent, args, { db, pubsub }, info) {
 		const postIndex = db.posts.findIndex((post) => post.id === args.id);
+
 		if (postIndex === -1) {
 			throw new Error("Post not found");
 		}
 
 		db.comments = db.comments.filter((comment) => comment.post !== args.id);
 
-		const deletedPost = db.posts.splice(postIndex, 1);
+		const [post] = db.posts.splice(postIndex, 1); // destructured the post array. this was previously deletedPost[0] when used in later calls
 
-		return deletedPost[0];
+		if (post.published) {
+			pubsub.publish("post", {
+				post: {
+					mutation: "DELETED",
+					data: post,
+				},
+			});
+		}
+
+		return post;
 	},
 	createComment(parent, args, { db, pubsub }, info) {
 		const userExists = db.users.some((user) => user.id === args.data.author);
@@ -140,17 +179,21 @@ const Mutation = {
 		};
 
 		db.comments.push(comment);
-    pubsub.publish(`comment ${args.data.post}`, { comment: comment })
+		pubsub.publish(`comment ${comment.post}`, {
+			comment: {
+				mutation: "CREATED",
+				data: comment,
+			},
+		});
 
 		return comment;
 	},
-	updateComment(parent, { id, data }, { db }, info) {
+	updateComment(parent, { id, data }, { db, pubsub }, info) {
 		const comment = db.comments.find((comment) => comment.id === id);
 		if (!comment) {
 			throw new Error("Comment not found");
 		}
 
-		console.log(data);
 		if (typeof data.text === "string") {
 			comment.text = data.text;
 		}
@@ -163,17 +206,32 @@ const Mutation = {
 			comment.post = data.post;
 		}
 
+		pubsub.publish(`comment ${comment.post}`, {
+			comment: {
+				mutation: "UPDATE",
+				data: comment,
+			},
+		});
+
 		return comment;
 	},
-	deleteComment(parent, args, { db }, info) {
+	deleteComment(parent, args, { db, pubsub }, info) {
 		const commentIndex = db.comments.findIndex((comment) => comment.id === args.id);
+
 		if (commentIndex === -1) {
 			throw new Error("Comment not found.");
 		}
 
-		const deletedComment = db.comments.splice(commentIndex, 1);
+		const [deletedComment] = db.comments.splice(commentIndex, 1);
 
-		return deletedComment[0];
+		pubsub.publish(`comment ${deletedComment.post}`, {
+			comment: {
+				mutation: "DELETED",
+				data: deletedComment,
+			},
+		});
+
+		return deletedComment;
 	},
 };
 
--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,20 +1,6 @@
 import { PubSub } from "graphql-yoga"
 
 const Subscription = {
-  count: {
-    subscribe(parent, args, { pubsub }, info) {
-      let count = 0
-
-      setInterval(()=> {
-        count++
-        pubsub.publish("count", {
-          count: count
-        })
-      }, 1000)
-
-      return pubsub.asyncIterator("count")
-    }
-  },
   comment: {
     subscribe(parent, { postId }, { db, pubsub }, info) {
       const post = db.posts.find((post) => post.id === postId && post.published)
