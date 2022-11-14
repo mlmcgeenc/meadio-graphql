ccff05040ddb1f0dc4e248d7abb9e93810f86d4a
Matt McGee <mlmcgeenc@gmail.com>
2022-11-13 19:17:07 -0500

    convert .then functions to async/await format

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -10,58 +10,84 @@ const prisma = new Prisma({
 // prisma.subscription
 // prisma.exists
 
-// prisma.query
-// 	.users(
-// 		null,
-// 		"{ id name email posts { id title comments { id author { name } text } } }"
-// 	)
-// 	.then((data) => {
-// 		console.log(JSON.stringify(data, undefined, 2));
-// 	});
+const createPostForUser = async (authorId, data) => {
+	const post = await prisma.mutation.createPost(
+		{
+			data: {
+				...data,
+				author: {
+					connect: {
+						id: authorId,
+					},
+				},
+			},
+		},
+		"{ id }"
+	);
+	const user = await prisma.query.user(
+		{
+			where: {
+				id: authorId,
+			},
+		},
+		"{ id name email posts{ id title published } }"
+	);
 
-// prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
-//   console.log(JSON.stringify(data,undefined, 2 ))
-// })
+	return user;
+};
+
+// createPostForUser("cla9x5ep401h00f60nfzxav20", {
+//   title: "Great books to read",
+//   body: "The War of Art",
+//   published: true
+// }).then((user) => console.log(JSON.stringify(user, undefined, 2)));
+
+const updatePostForUser = async (postId, data) => {
+	const post = await prisma.mutation.updatePost(
+		{
+			data: {
+				...data,
+			},
+			where: {
+				id: postId,
+			},
+		},
+		"{ author { id } }"
+	);
+	const user = await prisma.query.user(
+		{
+			where: {
+				id: post.author.id,
+			},
+		},
+		"{id name email posts{ id title published } }"
+	);
+
+	return user;
+};
+
+updatePostForUser("cla9xkprz01me0f609ofrxpds", {
+	title: "Updated title5",
+	body: "Updated Body5",
+	published: true,
+}).then((user) => console.log(JSON.stringify(user, undefined, 2)));
 
 // prisma.mutation
-// 	.createPost(
+// 	.updatePost(
 // 		{
 // 			data: {
-// 				title: "This is a second post from NODE",
-// 				body: "This post was created from a prisma.mutation call within NODE. It will be created each time the app gets run. The post is written by Kate.",
 // 				published: true,
-// 				author: {
-// 					connect: {
-// 						id: "cla9x93vv01ho0f60zukgyrqg",
-// 					},
-// 				},
+// 			},
+// 			where: {
+// 				id: "claa17dxq02a60f60sibi0jkz",
 // 			},
 // 		},
 // 		"{ id title body published }"
 // 	)
 // 	.then((data) => {
 // 		console.log(data);
-// 		return prisma.query.users(null, "{ id name email posts { id title } }");
-// 	}).then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2));
-//   })
-
-prisma.mutation
-	.updatePost(
-		{
-			data: {
-				published: true,
-			},
-			where: {
-				id: "claa17dxq02a60f60sibi0jkz",
-			},
-		},
-		"{ id title body published }"
-	)
-	.then((data) => {
-		console.log(data);
-		return prisma.query.posts(null, "{ id title body published }");
-	})
-	.then((data) => {
-		console.log(JSON.stringify(data, undefined, 2));
-	});
+// 		return prisma.query.posts(null, "{ id title body published }");
+// 	})
+// 	.then((data) => {
+// 		console.log(JSON.stringify(data, undefined, 2));
+// 	});
