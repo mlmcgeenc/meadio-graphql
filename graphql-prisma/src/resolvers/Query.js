6bd48043437826757ad341de498c9d4e450b44b9
Matt McGee <mlmcgeenc@gmail.com>
2022-12-06 09:47:01 -0500

    add opArgs to queries

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,50 +1,54 @@
 const Query = {
-		users(parent, args, { db, prisma }, info) {
-      return prisma.query.users(null, info)
+	users(parent, args, { prisma }, info) {
+		const opArgs = {};
 
-      // second arg can have nothing, string, object
-
-			// if (!args.query) {
-			// 	return db.users;
-			// }
-
-			// return db.users.filter((user) => {
-			// 	return db.user.name.toLowerCase().includes(args.query.toLowerCase());
-			// });
-		},
-		me() {
-			return {
-				id: "123098",
-				name: "Mike",
-				email: "mike@example.com",
-				age: 28,
+		if (args.query) {
+			opArgs.where = {
+				OR: [
+					{
+						name_contains: args.query,
+					},
+					{
+						email_contains: args.query,
+					},
+				],
 			};
-		},
-		post() {
-			return {
-				id: "1230987",
-				title: "Post Title",
-				body: "Post Body Text Goes here",
-				published: true,
+		}
+
+		return prisma.query.users(opArgs, info);
+	},
+	posts(parent, args, { prisma }, info) {
+		const opArgs = {};
+
+		if (args.query) {
+			opArgs.where = {
+				OR: [
+					{
+						title_contains: args.query,
+					},
+					{
+						body_contains: args.query,
+					},
+				],
 			};
-		},
-		posts(parent, args, { db, prisma }, info) {
-      return prisma.query.posts(null, info)
+		}
 
-			// if (!args.query) {
-			// 	return db.posts;
-			// }
+		return prisma.query.posts(opArgs, info);
 
-			// return db.posts.filter((post) => {
-			// 	return (
-			// 		post.title.toLowerCase().includes(args.query.toLowerCase()) ||
-			// 		post.body.toLowerCase().includes(args.query.toLowerCase())
-			// 	);
-			// });
-		},
-		comments(parent, args, { db }, info) {
-			return db.comments;
-		},
-	}
+		// if (!args.query) {
+		// 	return db.posts;
+		// }
 
-export { Query as default }
+		// return db.posts.filter((post) => {
+		// 	return (
+		// 		post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+		// 		post.body.toLowerCase().includes(args.query.toLowerCase())
+		// 	);
+		// });
+	},
+	comments(parent, args, { db }, info) {
+		return db.comments;
+	},
+};
+
+export { Query as default };
