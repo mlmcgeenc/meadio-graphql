3ed7c5ef99333dd8e14feae5e63990ac43c10af6
Matthew McGee <mlmcgeenc@gmail.com>
2022-12-07 19:50:21 -0500

    convert comments query to pull info from server and to use relational data

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -34,20 +34,9 @@ const Query = {
 		}
 
 		return prisma.query.posts(opArgs, info);
-
-		// if (!args.query) {
-		// 	return db.posts;
-		// }
-
-		// return db.posts.filter((post) => {
-		// 	return (
-		// 		post.title.toLowerCase().includes(args.query.toLowerCase()) ||
-		// 		post.body.toLowerCase().includes(args.query.toLowerCase())
-		// 	);
-		// });
 	},
-	comments(parent, args, { db }, info) {
-		return db.comments;
+	comments(parent, args, { prisma }, info) {
+		return prisma.query.comments(null, info);
 	},
 };
 
